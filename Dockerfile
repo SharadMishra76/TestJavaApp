8f922e8dab61d49b3b7f8dfb3e4f8e62104ba6d6
Sharad Mishra <cusat.sharad@gmail.com>
2017-10-23 11:13:00 +0530

    updated docker files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM java
 FROM maven:latest
-COPY . /usr/src/app
+#COPY . /usr/src/app
 RUN mvn clean install
 RUN curl -O http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz
 RUN tar xzf apache-tomcat-7.0.55.tar.gz
--- deployExec.sh
+++ deployExec.sh
@@ -2,5 +2,5 @@ docker run --rm -it maven mvn --version
 cd /
 cd /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
 #docker run  mvn clean install
-docker build -t testresteploy /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
+docker build -t testrestdeploy /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
 docker run -d --name testrestdeploycont -p 80:80 testrestdeploy 
