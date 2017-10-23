d3d7129d7de611277ee4ade73a850f05640a0cb1
Sharad Mishra <cusat.sharad@gmail.com>
2017-10-23 11:09:19 +0530

    updated docker files

--- deployExec.sh
+++ deployExec.sh
@@ -1,6 +1,6 @@
 docker run --rm -it maven mvn --version
 cd /
 cd /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
-docker run  mvn clean install
-docker build -t TestRestDeploy /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
-docker run -d --name TestRestDeployCont -p 80:80 TestRestDeploy 
+#docker run  mvn clean install
+docker build -t testresteploy /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
+docker run -d --name testrestdeploycont -p 80:80 testrestdeploy 
