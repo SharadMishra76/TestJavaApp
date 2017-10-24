523ad7bdc8b6340ec66fd41624c36618f3874f68
Sharad Mishra <cusat.sharad@gmail.com>
2017-10-24 11:04:22 +0530

    message

--- deployExec.sh
+++ deployExec.sh
@@ -3,5 +3,5 @@ docker run --rm -it maven mvn --version
 cd /
 cd /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
 #docker run  mvn clean install
-#docker build -t testrestdeploy2 /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
-#docker run -d --name testrestdeploycont2 -p 8080:8080 testrestdeploy2 
+docker build -t testrestdeploy2 /Users/sharadmishra/Documents/Sharad/gitJenkinProjects/TestJavaApp
+docker run -d --name testrestdeploycont2 -p 8080:8080 testrestdeploy2 
